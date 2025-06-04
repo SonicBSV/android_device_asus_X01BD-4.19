e365b97686bbd8dbfb61b02d798335dee6148783
SonicBSV <sonicbsv@ya.ru>
2025-06-04 18:28:53 +0300

    X01BD: drop thermal-engine config

--- device.mk
+++ device.mk
@@ -30,10 +30,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
-# Thermal configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # Wifi
 PRODUCT_PACKAGES += \
     X01BDWifiOverlay
