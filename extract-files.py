6b5f3f7a0b278917d76b1a8cf2ee1648758a15b5
SonicBSV <sonicbsv@ya.ru>
2025-08-01 14:32:40 +0300

    X01BD: update extract-utils on multi-device-py templates based

---
+++ extract-files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/asus/sdm660-common/qcom-caf',
+    'vendor/asus/sdm660-common',
+]
+
+# Define the blob fixups
+blob_fixups: blob_fixups_user_type = {
+    # Camera - uneeded
+    'vendor/lib/libmmcamera_tuning.so': blob_fixup()
+        .remove_needed('libmm-qcamera.so'),
+}  # fmt: skip
+
+# Define the module
+module = ExtractUtilsModule(
+    'X01BD',
+    'asus',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sdm660-common', module.vendor)
+    utils.run()
