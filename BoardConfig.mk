fa26b5d8dbc30f040de51f41aadef4bf5c012f4d
SonicBSV <sonicbsv@ya.ru>
2026-01-15 08:10:54 +0300

    X01BD: set build host/user and change wake node to "/proc/tpd_gesture"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,9 +21,11 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := asus/X01BD_defconfig
+TARGET_KERNEL_BUILD_HOST := beastmachine
+TARGET_KERNEL_BUILD_USER := "SonicBSV"
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/kernel/touchpanel/dclicknode"
+TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
